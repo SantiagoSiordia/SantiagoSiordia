3ac942293c2ad559a9ef5e85c01c4bd0cd89a97e
Santiago González Siordia <santiago.gonzalez.siordia@gmail.com>
2020-07-31 21:20:04 -0500

    Updates

---
+++ precache-manifest.bf693154fc9269f9083ba189377ae04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f98941b9b3e873f8df5a4ce3cf5793a",
+    "url": "/Santiago/index.html"
+  },
+  {
+    "revision": "840a4791102c766242de",
+    "url": "/Santiago/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "00c7fdf0215a0da035c7",
+    "url": "/Santiago/static/js/2.1caff5ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Santiago/static/js/2.1caff5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840a4791102c766242de",
+    "url": "/Santiago/static/js/main.fdfbff16.chunk.js"
+  },
+  {
+    "revision": "4a951f408ff128126064",
+    "url": "/Santiago/static/js/runtime-main.85c5782d.js"
+  }
+]);
